24f7669a1588ed5438d4bd44c0d5a1173516cc02
sdf-hgnom <shulinab@yandex.ru>
2020-07-21 11:01:40 +0800

    add classes in client.py

--- src/client.py
+++ src/client.py
@@ -3,10 +3,25 @@ from twisted.protocols.basic import LineOnlyReceiver
 
 from .server import TCP_PORT
 
+
 class ConnectorProtocol(LineOnlyReceiver):
     factory: "Connector"
-    pass
+
+    def connectionMade(self):
+        self.factory.window.protocol = self
+        pass
+
+    def lineReceived(self, line):
+        message = line.decode(encoding='utf-8')
+        pass
+
 
 class Connector(ClientFactory):
-    protocol:ConnectorProtocol
+    window: 'ChatWindow'
+    protocol: ConnectorProtocol
     pass
+
+
+class ChatWindow:
+    protocol: ConnectorProtocol
+    reactor = None
