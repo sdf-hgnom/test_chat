141a5420e3978989096ee156a964d47eba12586b
sdf-hgnom <shulinab@yandex.ru>
2020-07-23 01:40:06 +0800

    add test_gui.py

--- src/gui/test_gui.py
+++ src/gui/test_gui.py
@@ -1,9 +1,29 @@
-class Test:
-    pass
+import sys
+
+from PyQt5.QtCore import QRect
+from PyQt5.QtWidgets import QApplication, QWidget
+from PyQt5.QtGui import QFont
+class Test(QWidget):
+    qr:QRect
+
+    def center(self):
+        qr = self.frameGeometry()
 
 
-def main():
-    pass
+# def main():
+#     app = QApplication(sys.argv)
+#     w = QWidget()
+#     w.sef
+#     w.resize(250, 150)
+#     w.move(300, 300)
+#     w.setWindowTitle('Simple')
+#     w.show()
+#     sys.exit(app.exec_())
 
 if __name__ == '__main__':
-    main()
+    s = iter('qwerty12')
+    for i in s:
+        pass
+    print('tt',*s)
+
+    # main()
